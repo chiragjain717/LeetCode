7d3dcd7fe94d7d999b459dc76048f506548ad190
CHIRAG JAIN <cjain9020@gmail.com>
2026-08-05 18:52:50 +0530

    Time: 254 ms (21.76%) | Memory: 46.9 MB (28.54%) - LeetSync

--- 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.java
+++ 2594-count-pairs-of-similar-strings/count-pairs-of-similar-strings.java
@@ -1,12 +1,12 @@
 class Solution {
     public int similarPairs(String[] words) {
-         int m = 0;
+          int m = 0;
        for (int i = 0; i < words.length; i++) {
+       
          Set<Character> set1 = new HashSet<>();
             for (char ch : words[i].toCharArray()) {
                 set1.add(ch);
             }
-
             for (int j = i + 1; j < words.length; j++) {
                 Set<Character> set2 = new HashSet<>();
                 for (char ch : words[j].toCharArray()) {
